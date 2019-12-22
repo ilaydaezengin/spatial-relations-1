1064ab8308af2582d04c12400dcca5a0a78bb611
EC2 Default User <ec2-user@ip-172-31-25-218.us-east-2.compute.internal>
2019-12-22 10:05:45 +0000

    First trained natural language model 5-epochs

--- train_dream.jl
+++
@@ -1,9 +0,0 @@
-function train(train_data, )
-    for env in train_data
-        r = encode(env)
-        im_pred = generate(thetas, r)
-        # thetas = 10 element array of tuples [x,y,z], im_pred = 10-el array of matrices/predicted images
-        l = loss(im_pred, im_gold)
-        updateweights!(l, )
-    end
-end
